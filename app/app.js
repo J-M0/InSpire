aaa983704cafad74832e3460eb292b26b808574d
Kevin Chan <kcchan@umass.edu>
2016-03-06 12:23:56 -0500

    Removed the execessive bundle.js call, added the app.js call Edit app.js to add your React components

--- app/app.js
+++ app/app.js
@@ -1,1 +1,14 @@
-/* Empty for now. :) */
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class UghWorld extends React.Component {
+	render () {
+		return (
+			<p>ugh, world</p>
+		);
+	}
+}
+
+ReactDOM.render(
+	<UghWorld />, document.getElementById('top_container')
+);
