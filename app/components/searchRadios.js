c3b49e5120117c5709a2dbd50e5f3c782bd5daf1
James Morris <jamesmorris2@gmail.com>
2016-03-08 16:50:03 -0500

    Made searchRadios compliant with React

--- app/components/searchRadios.js
+++ app/components/searchRadios.js
@@ -1,53 +1,53 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-export default class SearchRadios {
+export default class SearchRadios extends React.Component {
 	render() {
 		return (
 			<div>
-				<div class="panel-heading" style="color:#354066;">Class Search Filters</div>
-				<div class="checkbox">
-					<label><input type="checkbox" value="">Seats Available</label>
+				<div className="panel-heading" style={{color: '#354066'}}>Class Search Filters</div>
+				<div className="checkbox">
+					<label><input type="checkbox" value="" />Seats Available</label>
 				</div>
-				<div class="form-group">
-					<label for="keyword">Keyword:</label>
-					<input type="text" class="class-num pull-right keyword">
+				<div className="form-group">
+					<label htmlFor="keyword">Keyword:</label>
+					<input type="text" className="class-num pull-right keyword" />
 				</div>
-				<div class="form-group">
-					<label for="class-num">Class Number:</label>
-					<select class="form control" id="course-num-ops">
-						<option>=</option>
+				<div className="form-group">
+					<label htmlFor="class-num">Class Number:</label>
+					<select className="form control" id="course-num-ops">
+						<option>{"="}</option>
 						<option>&gt;=</option>
-						<input type="text" class="class-num pull-right keyword">
 					</select>
+					<input type="text" className="class-num pull-right keyword" />
 				</div>
-				<div class="form-group">
-					<label for="subject">Subject:</label>
-					<select class="form-control" id="subject">
+				<div className="form-group">
+					<label htmlFor="subject">Subject:</label>
+					<select className="form-control" id="subject">
 						<option>Computer Science</option>
 					</select>
 				</div>
-				<div class="form-group">
-					<label for="gen-ed-cat">Gen Ed Category:</label>
-					<select class="form-control" id="gen-ed-cat">
+				<div className="form-group">
+					<label htmlFor="gen-ed-cat">Gen Ed Category:</label>
+					<select className="form-control" id="gen-ed-cat">
 						<option>AL Literature</option>
 					</select>
 				</div>
-				<div class="form-group">
-					<label for="session">Session</label>
-					<select class="form-control" id="session">
+				<div className="form-group">
+					<label htmlFor="session">Session</label>
+					<select className="form-control" id="session">
 						<option>*University</option>
 					</select>
 				</div>
-				<div class="form-group">
-					<label for="instr-mode">Mode of Instruction</label>
-					<select class="form-control" id="instr-mode">
+				<div className="form-group">
+					<label htmlFor="instr-mode">Mode of Instruction</label>
+					<select className="form-control" id="instr-mode">
 						<option>Classroom</option>
 					</select>
 				</div>
-				<div class="center-block">
-					<button id="singlebutton" name="singlebutton" class="btn btn-primary center-block" style="background-color:#354066;">
-						Search <span class="glyphicon glyphicon-search" style="margin-left:5px;margin-bottom:5px;"></span>
+				<div className="center-block">
+					<button id="singlebutton" name="singlebutton" className="btn btn-primary center-block" style={{backgroundColor: '#354066'}}>
+						Search <span className="glyphicon glyphicon-search" style={{marginLeft: '5px', marginBottom: '5px'}}></span>
 					</button>
 				</div>
 			</div>
