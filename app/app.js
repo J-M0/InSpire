c0db11b2feef5c5f9e28b3b095b8dc6093d28a5b
Kevin Chan <kcchan@umass.edu>
2016-03-08 18:15:06 -0500

    Adding functionality to react-router

--- app/app.js
+++ app/app.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Calendar from './components/calendarBlock';
+import SideNav from './components/Navbar';
 import { IndexRoute, Router, Route, hashHistory } from 'react-router';
 
 class App extends React.Component {
 	render() {
 		return (
-			<div>{this.props.children}</div>
+			<div>
+				{this.props.Calendar}
+				{/*this.props.SideNav*/}
+			</div>
 		);
 	}
 }
@@ -14,8 +18,8 @@ class App extends React.Component {
 ReactDOM.render((
 	<Router history={hashHistory}>
 		<Route path="/" component={App}>
-			<IndexRoute component={Calendar}/>
-			<Route path="/profile/:id" component={Calendar} />
+			{/*<IndexRoute component={}/>*/}
+			<Route path="/profile/:id" components={{Calendar, SideNav}} />
 		</Route>
 	</Router>
 ),document.getElementById('calendar'));
