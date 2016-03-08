23b1c59a14ded531f1cb5998970b85eb94d40103
James Morris <jamesmorris2@gmail.com>
2016-03-08 15:30:51 -0500

    made searchResults HTML compliant with React

--- app/components/searchResults.js
+++ app/components/searchResults.js
@@ -1,49 +1,55 @@
 import React from "react";
+import ReactDOM from "react-dom";
 
 export default class SearchResults extends React.Component {
 	render() {
 		return (
 			<div>
-				<div class="panel-heading" style="color:354066;"><span class="glyphicon glyphicon-chevron-left" style="color:#354066;"></span> Search Results</div>
-				<ul class="list group">
-					<li class="list-group-item">
-						<span class="glyphicon glyphicon-asterisk" style="color:green;"></span>
-						CS 326 - Web Programming <span class="glyphicon glyphicon-chevron-down pull-right"></span>
-						<br>
-						<br>
+				<div className="panel-heading" style={{color: '#354066'}}><span className="glyphicon glyphicon-chevron-left" style={{color: '#354066'}}></span> Search Results</div>
+				<ul className="list group">
+					<li className="list-group-item">
+						<span className="glyphicon glyphicon-asterisk" style={{color: 'green'}}></span>
+						CS 326 - Web Programming <span className="glyphicon glyphicon-chevron-down pull-right"></span>
+						<br />
+						<br />
 						THIS IS A COOL CLASS. TRUST ME.
-						<a class="btn" data-toggle="modal" href="#CS326Modal" style="text-align:right;">...More info</a>
+						<a className="btn" data-toggle="modal" href="#CS326Modal" style={{textAlign: 'right'}}>...More info</a>
 					</li>
-					<li class="list-group-item">
-						<span class="glyphicon glyphicon-play class-full-btn" style="color:blue;"></span>
-						CS 320 - Software Engineering <span class="glyphicon glyphicon-chevron-right pull-right"></span>
+					<li className="list-group-item">
+						<span className="glyphicon glyphicon-play class-full-btn" style={{color: 'blue'}}></span>
+						CS 320 - Software Engineering <span className="glyphicon glyphicon-chevron-right pull-right"></span>
 					</li>
-					<li class="list-group-item">
-						<span class="glyphicon glyphicon-stop" style="color:yellow;"></span>
-						CS 373 - Intro. to Computer Graphics <span class="glyphicon glyphicon-chevron-right pull-right"></span>
+					<li className="list-group-item">
+						<span className="glyphicon glyphicon-stop" style={{color: 'yellow'}}></span>
+						CS 373 - Intro. to Computer Graphics <span className="glyphicon glyphicon-chevron-right pull-right"></span>
 					</li>
-					<li class="list-group-item">
-						<span class="glyphicon glyphicon-asterisk" style="color:green;"></span>
-						CS 383 - Artificial Intelligence <span class="glyphicon glyphicon-chevron-right pull-right"></span>
+					<li className="list-group-item">
+						<span className="glyphicon glyphicon-asterisk" style={{color: 'green'}}></span>
+						CS 383 - Artificial Intelligence <span className="glyphicon glyphicon-chevron-right pull-right"></span>
 					</li>
-					<li class="list-group-item">
-						<span class="glyphicon glyphicon-asterisk" style="color:green;"></span>
-						CS 377 - Operating Systems <span class="glyphicon glyphicon-chevron-down pull-right"></span>
-						<br>
-						<br>
+					<li className="list-group-item">
+						<span className="glyphicon glyphicon-asterisk" style={{color: 'green'}}></span>
+						CS 377 - Operating Systems <span className="glyphicon glyphicon-chevron-down pull-right"></span>
+						<br />
+						<br />
 						MORE INFO ABOUT ANOTHER CLASS OR SOMETHING.
-						<a class="btn" data-toggle="modal" href="#CS377Modal" style="text-align:right;">...More info</a>
+						<a className="btn" data-toggle="modal" href="#CS377Modal" style={{textAlign: 'right'}}>...More info</a>
 					</li>
-					<li class="list-group-item">
-						<span class="glyphicon glyphicon-asterisk" style="color:green;"></span>
-						CS 311 - Algorithms <span class="glyphicon glyphicon-chevron-right pull-right"></span>
+					<li className="list-group-item">
+						<span className="glyphicon glyphicon-asterisk" style={{color: 'green'}}></span>
+						CS 311 - Algorithms <span className="glyphicon glyphicon-chevron-right pull-right"></span>
 					</li>
-					<li class="list-group-item">
-						<span class="glyphicon glyphicon-asterisk" style="color:green;"></span>
-						CS 365 - Digital Forensics <span class="glyphicon glyphicon-chevron-right pull-right"></span>
+					<li className="list-group-item">
+						<span className="glyphicon glyphicon-asterisk" style={{color: 'green'}}></span>
+						CS 365 - Digital Forensics <span className="glyphicon glyphicon-chevron-right pull-right"></span>
 					</li>
 				</ul>
 			</div>
 		)
 	}
 }
+
+ReactDOM.render(
+	<SearchResults />,
+	document.getElementById('search-results')
+);
