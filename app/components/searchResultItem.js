552c54498df574ac84d646e7e4387193afeadf4f
James Morris <jamesmorris2@gmail.com>
2016-03-12 21:18:47 -0500

    Flippy arrows on results work. Spacing is weird though

--- app/components/searchResultItem.js
+++ app/components/searchResultItem.js
@@ -4,29 +4,45 @@ export default class SearchResultItem extends React.Component {
 	constructor(props) {
 		super(props);
 
-		this.state = props.data;
+		var newState = props.data;
+		newState.moreInfo = false;
+		this.state = newState;
 	}
 
 	handleChevronClick(clickEvent) {
 		clickEvent.preventDefault();
 
 		if(clickEvent.button === 0) {
-
+			this.setState({moreInfo: !this.state.moreInfo});
 		}
 	}
 
 	render() {
 		var data = this.state;
+		var body;
+
+		if(this.state.moreInfo)
+		{
+			body = (
+				<div>
+					<a href="#" onClick={(e) => this.handleChevronClick(e)}><span className="glyphicon glyphicon-chevron-down pull-right"></span></a>
+					<br />
+					<br />
+					{data.description}
+					<a className="btn" data-toggle="modal" href="#Modal" style={{textAlign: 'right'}}>...More info</a>
+				</div>
+			);
+		} else {
+			body = (
+				<a href="#" onClick={(e) => this.handleChevronClick(e)}><span className="glyphicon glyphicon-chevron-right pull-right"></span></a>
+			);
+		}
+
 		return (
 			<li className="list-group-item">
 				<span className="glyphicon glyphicon-asterisk" style={{color: 'green'}}></span>
-				{data.courseId} - {data.courseName}<a href="#"><span className="glyphicon glyphicon-chevron-left pull-right"></span></a>
+				{data.courseId} - {data.courseName} {body}
 			</li>
 		);
 	}
 }
-
-// <br />
-// <br />
-// MORE INFO ABOUT ANOTHER CLASS OR SOMETHING.
-// <a className="btn" data-toggle="modal" href="#Modal" style={{textAlign: 'right'}}>...More info</a>
