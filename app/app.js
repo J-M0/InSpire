08c90ff9c55e83e782f273958d0c8e8c1e819dce
Michael Gallant <mgallant@umass.edu>
2016-03-13 14:29:07 -0400

    merge conflict

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import Calendar from './components/calendarBlock';
 import SideNav from './components/navbar';
 import ShoppingCart from './components/shoppingCart';
+import ImportantNotices from './components/importantNotices'
 import { IndexRoute, Router, Route, hashHistory } from 'react-router';
 
 /**
@@ -19,97 +20,6 @@ class App extends React.Component {
       this.props.ShoppingCart.props.params.id=12345678;
     return (
       <div id="application_wrapper">
-        <div id="todoModal" className="modal fade" role="dialog">
-          <div className="modal-dialog modal-lg">
-            <div className="modal-content">
-              <div className="modal-header">
-                <button type="button" className="close" data-dismiss="modal">&times;</button>
-                <h4 className="modal-title" style={{color:'#354066'}}>Todo list & More</h4>
-              </div>
-              <div className="modal-body">
-                <div className="panel panel-default" id="todo-list-items">
-                  <div className="panel-heading" style={{color:'#354066'}}>
-                    <span className="glyphicon glyphicon-ok-circle" style={{color:'#354066'}}></span> Todo Items
-                  </div>
-                  <div className="panel-body">
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th>Todo Item</th>
-                          <th>Due Date</th>
-                          <th>Status</th>
-                          <th>Institution</th>
-                          <th>Administrative Function</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td>2016-2017 FAFSA</td>
-                          <td>03/01/2015</td>
-                          <td>Received</td>
-                          <td>U. of Massachusetts Amherst</td>
-                          <td>Financial Aid</td>
-                        </tr>
-                        <tr>
-                          <td>Graduation Paperwork</td>
-                          <td>03/01/2015</td>
-                          <td>Received</td>
-                          <td>U. of Massachusetts Amherst</td>
-                          <td>Graduate Program</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                  <div className="panel-heading" style={{color:'#354066'}}>
-                    <span className="glyphicon glyphicon-remove-circle" style={{color:'#354066'}}></span> Holds
-                  </div>
-                  <p style={{marginLeft:'20px'}}>There are currently no holds on your account.</p>
-                  <br/>
-                  <div className="panel-heading" style={{color:'#354066'}}>
-                    <span className="glyphicon glyphicon-gift" style={{color:'#354066'}}></span> Financial Aid
-                  </div>
-                  <div className="panel-body">
-                    <p>Select the year of aid you wish to view.</p>
-                    <table className="table">
-                      <thead>
-                        <tr>
-                          <th>Aid Year</th>
-                          <th>Institution</th>
-                          <th>Aid Year Description</th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        <tr>
-                          <td><a href="#">2016</a></td>
-                          <td>U. of Massachusetts Amherst</td>
-                          <td>Financial Aid Year 2015-2016</td>
-                        </tr>
-                        <tr>
-                          <td><a href="#">2015</a></td>
-                          <td>U. of Massachusetts Amherst</td>
-                          <td>Financial Aid Year 2014-2015</td>
-                        </tr>
-                        <tr>
-                          <td><a href="#">2014</a></td>
-                          <td>U. of Massachusetts Amherst</td>
-                          <td>Financial Aid Year 2013-2014</td>
-                        </tr>
-                        <tr>
-                          <td><a href="#">2013</a></td>
-                          <td>U. of Massachusetts Amherst</td>
-                          <td>Financial Aid Year 2012-2013</td>
-                        </tr>
-                      </tbody>
-                    </table>
-                  </div>
-                </div>
-              </div>
-              <div className="modal-footer">
-                <button className="btn btn-default" data-dismiss="modal">Close</button>
-              </div>
-            </div>
-          </div>
-        </div>
 
         <div className="modal fade" tabIndex="-1" role="dialog" id="classNameInfoModal">
           <div className="modal-dialog">
@@ -192,16 +102,7 @@ class App extends React.Component {
 
         <div className="row" id="top_container">
           <div className="col-md-3" id="side-navbar">
-            <div className="panel panel-default" id="todo">
-              <div className="panel-heading">
-                Important Notices
-              </div>
-              <div className="panel-body">
-                <a href="#fafsa">2016-2017 FAFSA</a><br/>
-                <a href="#grad">Graduation Paperwork</a><br/>
-                <a href="#etc" data-toggle="modal" data-target="#todoModal">See More...</a><br/>
-              </div>
-            </div>
+            {this.props.ImportantNotices}
 
             <ul id="menu-tabs" className="nav nav-pills search-tabs" data-tabs="tab-content">
               <li className="active"><a data-toggle="tab" href="#search">Search</a></li>
@@ -299,7 +200,7 @@ ReactDOM.render((
   <Router history={hashHistory}>
     <Route path="/" component={App}>
       {/* EXAMPLE: {{Calendar, SideNav, SearchPanel, etc., etc., etc.,}} */}
-      <IndexRoute components={{Calendar, SideNav, ShoppingCart}}/>
+      <IndexRoute components={{Calendar, SideNav, ShoppingCart, ImportantNotices}}/>
       {/*DO NOT UNCOMMENT THE BELOW UNLESS YOUR NAME IS KEVIN CHAN */}
       {/*<Route path="/profile/:id" components={{Calendar, SideNav}} />*/}
     </Route>
