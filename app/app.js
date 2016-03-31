b6d0d70852fd6a5f53331b5535ba7af77787fd4d
Nick Merlino <nmerlino@umass.edu>
2016-03-31 18:05:17 -0400

    Removed sidenav and converted to UserInfo box style

--- app/app.js
+++ app/app.js
@@ -9,20 +9,20 @@ import SearchPanel from './components/searchPanel';
 import { IndexRoute, Router, Route, hashHistory } from 'react-router';
 
 /**
-  * Waiting on course information modal as well as search before i can implement shopping cart
-  * Stephen P
-  */
+* Waiting on course information modal as well as search before i can implement shopping cart
+* Stephen P
+*/
 
 class App extends React.Component {
   render() {
     // Default user, to be removed
     if (this.props.Calendar.props.params.id === undefined)
-      this.props.Calendar.props.params.id=12345678;
+    this.props.Calendar.props.params.id=12345678;
 
     return (
-		  <div className="row" id="top_container">
+      <div className="row" id="top_container">
 
-        {this.props.SideNav}
+        //{this.props.SideNav}
 
         <div className="col-md-3" id="left-side">
           {this.props.ImportantNotices}
@@ -50,9 +50,9 @@ class App extends React.Component {
 }
 
 class SearchPanelWrapper extends React.Component {
-    render() {
-        return <SearchPanel view={'search'} />
-    }
+  render() {
+    return <SearchPanel view={'search'} />
+  }
 }
 
 ReactDOM.render((
