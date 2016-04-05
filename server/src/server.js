dab43674ba3057c2070c6c3ac62eaca968ac19ea
James Morris <jamesmorris2@gmail.com>
2016-04-05 11:57:10 -0400

    Beginning to write server

--- server/src/server.js
+++ server/src/server.js
@@ -1,2 +1,20 @@
 // Implement your server in this file.
 // We should be able to run your server with node src/server.js
+var express = require('express');
+var validate = require('express-jsonschema').validate;
+var bodyParser = require('body-parser');
+var database = require('./database');
+var readDocument = database.readDocument;
+var writeDocument = database.writeDocument;
+var addDocument = database.addDocument;
+
+var app = express();
+app.use(bodyParser.text());
+app.use(bodyParser.json());
+
+app.use(express.static('../client/build'));
+
+
+app.listen(3000, function() {
+	console.log('InSpire server listening on port 3000!');
+});
