29fb9ac7969c2d981a92931f26983dde63f7affb
Kevin Chan <kcchan@umass.edu>
2016-04-07 15:13:52 -0400

    removed the client/database

--- client/app/server.js
+++ client/app/server.js
@@ -1,15 +1,3 @@
-// import {readDocument, readDocuments , writeDocument} from './database.js';
-
-/**
- * Emulates how a REST call is *asynchronous* -- it calls your function back
- * some time in the future with data.
- */
-function emulateServerReturn(data, cb) {
-  setTimeout(() => {
-    cb(data);
-  }, 4);
-}
-
 var token = 'eyJpZCI6MTIzNDU2Nzh9';
 
 /**
