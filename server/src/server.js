7f4904821a61f3a2a851a07ed28c75edd0d7a6f2
Kevin Chan <kcchan@umass.edu>
2016-04-19 18:26:09 -0400

    fixed catastrophe

--- server/src/server.js
+++ server/src/server.js
@@ -7,9 +7,9 @@ var searchSchema  = require('./schemas/searchOptions.json');
 
 // Mock db-variables, we should be able to delete these soon
 var database = require('./database');
-var ResetDatabase = require('./resetdatabase');
 var readDocument = database.readDocument;
 var writeDocument = database.writeDocument;
+var addDocument = database.addDocument;
 // Delete the above
 
 // MongoDB variables
@@ -32,12 +32,11 @@ MongoClient.connect(databaseUrl, function(err, db) {
     res.status(500).send("A database error occurred: " + err);
   }
 
-  // Reset the database.
+  // Reset database.
   app.post('/resetdb', function(req, res) {
     console.log("Resetting database...");
-    ResetDatabase(db, function() {
-      res.send();
-    });
+    database.resetDatabase();
+    res.send();
   });
 
   // Search for classes
@@ -54,21 +53,21 @@ MongoClient.connect(databaseUrl, function(err, db) {
     for (var k in body) {
       switch(k) {
         case 'keyword':
-        if (body[k].length !== 0)
-        query['$text']= {$search: body[k]};
-        break;
+          if (body[k].length !== 0)
+            query['$text']= {$search: body[k]};
+          break;
         case 'seatsAvail':
-        if (body[k]) {
-          query['$where'] = 'this.enrolled.length < this.capacity';
-        }
-        break;
+          if (body[k]) {
+            query['$where'] = 'this.enrolled.length < this.capacity';
+          }
+          break;
         case 'opFilter':
-        query['courseNumber']=body[k]['courseNumber'];
-        break;
+          query['courseNumber']=body[k]['courseNumber'];
+          break;
         default:
-        if (body[k].length !== 0) {
-          query[k] = body[k];
-        }
+          if (body[k].length !== 0) {
+            query[k] = body[k];
+          }
       }
     }
 
@@ -98,60 +97,60 @@ MongoClient.connect(databaseUrl, function(err, db) {
     var courseId = new ObjectID(urlObj.query.course);
 
     if(fromUser.equals(studentId)) {
-      db.collection('students').findOne({ _id: studentId }, { enrolledCourses: 1 }, function(err, results) {
-        // console.log(results);
-        var query = {
-          $or: results.enrolledCourses.map((id) => { return { _id: id } })
-        }
-        db.collection('courses').find(query).toArray(function(err, courses) {
-          if(err) {
-            return sendDatabaseError(res, err);
-          }
-
-          db.collection('courses').findOne({ _id: courseId }, function(err, enrollingCourse) {
-            if(err) {
-              return sendDatabaseError(res, err);
+        db.collection('students').findOne({ _id: studentId }, { enrolledCourses: 1 }, function(err, results) {
+            // console.log(results);
+            var query = {
+                $or: results.enrolledCourses.map((id) => { return { _id: id } })
             }
-
-            var canEnroll = true;
-
-            for(var i in courses) {
-              if(coursesConflict(courses[i], enrollingCourse)) {
-                canEnroll = false;
-                break;
-              }
-            }
-
-            if(canEnroll) {
-              db.collection('courses').updateOne({ _id: courseId }, {
-                $addToSet: {
-                  enrolled: studentId
-                }
-              }, function(err, result) {
+            db.collection('courses').find(query).toArray(function(err, courses) {
                 if(err) {
-                  return sendDatabaseError(res, err);
+                    return sendDatabaseError(res, err);
                 }
 
-                // console.log(result);
+                db.collection('courses').findOne({ _id: courseId }, function(err, enrollingCourse) {
+                    if(err) {
+                        return sendDatabaseError(res, err);
+                    }
 
-                db.collection('students').updateOne({ _id: studentId }, {
-                  $addToSet: {
-                    enrolledCourses: courseId
-                  }
-                }, function(err) {
-                  if(err) {
-                    return sendDatabaseError(res, err);
-                  }
+                    var canEnroll = true;
 
-                  res.send();
+                    for(var i in courses) {
+                        if(coursesConflict(courses[i], enrollingCourse)) {
+                            canEnroll = false;
+                            break;
+                        }
+                    }
+
+                    if(canEnroll) {
+                        db.collection('courses').updateOne({ _id: courseId }, {
+                            $addToSet: {
+                                enrolled: studentId
+                            }
+                        }, function(err, result) {
+                            if(err) {
+                                return sendDatabaseError(res, err);
+                            }
+
+                            // console.log(result);
+
+                            db.collection('students').updateOne({ _id: studentId }, {
+                                $addToSet: {
+                                    enrolledCourses: courseId
+                                }
+                            }, function(err) {
+                                if(err) {
+                                    return sendDatabaseError(res, err);
+                                }
+
+                                res.send();
+                            });
+                        });
+                    } else {
+                        res.status(400).send("Could not enroll in class. There is a time conflict.")
+                    }
                 });
-              });
-            } else {
-              res.status(400).send("Could not enroll in class. There is a time conflict.")
-            }
-          });
+            });
         });
-      });
     } else {
       res.status(401).end();
     }
@@ -161,9 +160,9 @@ MongoClient.connect(databaseUrl, function(err, db) {
     var haveCommonDays = course1.days.some((day1) => course2.days.some((day2) => day1 === day2));
 
     if(haveCommonDays) {
-      return (course1.start <= course2.start && course2.end <= course1.end);
+        return (course1.start <= course2.start && course2.end <= course1.end);
     } else {
-      return false;
+        return false;
     }
   }
 
@@ -187,189 +186,192 @@ MongoClient.connect(databaseUrl, function(err, db) {
           }
 
           db.collection('students').updateOne({ _id: new ObjectID(studentId) }, { $pull: { enrolledCourses: courseId } },
-          function(err) {
-            if(err) {
-              return sendDatabaseError(res, err);
+            function(err) {
+              if(err) {
+                return sendDatabaseError(res, err);
+              }
+
+              res.send();
+
             }
-
-            res.send();
-
-          }
-        );
-      }
-    );
-  } else {
-    res.send(401).end();
-  }
-});
-
-// MADE CHANGES HERE
-// POST request for removing a class from the shopping cart
-app.post('/dropfromcart', function(req, res) {
-
-  var student = readDocument('students', req.body.userId);
-  var course = req.body.courseId;
-
-  var fromUser = getUserIdFromToken(req.get('Authorization'));
-
-  if (fromUser == req.body.userId) {
-    var courseIndex = student.cart.indexOf(course);
-    if (courseIndex !== -1 && student.cart.length !== 0) {
-      student.cart.splice(courseIndex, 1);
+          );
+        }
+      );
     } else {
-      res.status(500).end();
+      res.send(401).end();
     }
-
-    writeDocument('students', student);
-
-    for (var i = 0, cart=[]; i < student.cart.length; i++) {
-      cart.push(readDocument('courses', student.cart[i]));
-    }
-
-    res.send(cart);
-
-  } else {
-    res.status(401).end();
-  }
-});
-
-// GET request for course information
-// Nick is doing this
-app.get('/courses/:courseid', function(req, res) {
-  var courseId = new ObjectID(req.params.courseid);
-
-  db.collection('courses').findOne({_id : courseId}, function (err, course) {
-    if (err) sendDatabaseError(res, err);
-    else res.send(course);
   });
-});
 
-// GET request for available courses
-// May become more difficult when we extend functionality
-// Nick is doing this
-app.get('/courses/available/:day/:start', function(req, res) {
-  var time = new Date(req.params.start);
-  var day = req.params.day;
+  // MADE CHANGES HERE
+  // POST request for removing a class from the shopping cart
+  app.post('/dropfromcart', function(req, res) {
 
-  var available = [];
-  var cursor = db.collection('courses').find({start : time}, {days : {$in : day}});
-  cursor.forEach( function(course) {
-    var courseBlock = new Date(req.params.start);
-    if (course.start < courseBlock && course.end > courseBlock) available.push(course);
-  }, function () {
+    var student = readDocument('students', req.body.userId);
+    var course = req.body.courseId;
+
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+
+    if (fromUser == req.body.userId) {
+      var courseIndex = student.cart.indexOf(course);
+      if (courseIndex !== -1 && student.cart.length !== 0) {
+        student.cart.splice(courseIndex, 1);
+      } else {
+        res.status(500).end();
+      }
+
+      writeDocument('students', student);
+
+      for (var i = 0, cart=[]; i < student.cart.length; i++) {
+        cart.push(readDocument('courses', student.cart[i]));
+      }
+
+      res.send(cart);
+
+    } else {
+      res.status(401).end();
+    }
+  });
+
+  // GET request for course information
+  app.get('/courses/:courseid', function(req, res) {
+    //var course = readDocument('courses', req.params.courseid);
+    //course.instructor = readDocument('professor', course.instructor);
+
+    //res.send(course);
+  });
+
+  // GET request for available courses
+  // May become more difficult when we extend functionality
+  app.get('/courses/available/:day/:start', function(req, res) {
+    var available = [];
+    var courses = database.getCollection('courses');
+
+    var blockStart = new Date(req.params.start);
+    for (var i in courses) {
+      var courseStart = new Date(courses[i].start);
+      var courseEnd   = new Date(courses[i].end);
+      courses[i].days.map((d) => {
+        if (req.params.day === d) {
+          if (courseStart <= blockStart && courseEnd >= blockStart) {
+            available.push(courses[i]);
+          }
+        }
+      });
+    }
     res.send(available);
   });
-});
 
-// GET request for student's enrolled courses
-app.get('/students/:studentid/enrolled', function(req, res) {
-  var id = new ObjectID(req.params.studentid);
-  var fromUser = getUserIdFromToken(req.get('Authorization'));
+  // GET request for student's enrolled courses
+  app.get('/students/:studentid/enrolled', function(req, res) {
+    var id = new ObjectID(req.params.studentid);
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
 
-  if (id == fromUser) {
-    db.collection('students').findOne({_id : id}, function (err, student) {
-      if (err) {
-        sendDatabaseError(res, err);
-      } else {
-        var courses = [];
-        // A cursor is analogous to a pointer from C/C++. You need to iterate over the cursor object
-        // which is a lot like a LinkedList from Java.
-        var cursor = db.collection('courses').find({_id: {$in : student.enrolledCourses}});
-        cursor.forEach( function(doc) {                 // From the Node.js MongoDB driver API
-          courses.push(doc);                            // forEach takes 2 functions as parameters
-        }, function () {                                // First function is applied every iteration
-          res.send(courses);                            // Second function is applied at the end
-        });
-      }
-    });
-  } else {
-    res.status(400).end();
-  }
-});
-
-// GET request for student shopping cart
-app.get('/students/:studentid/cart', function(req, res) {
-  var id = req.params.studentid;
-  var fromUser = getUserIdFromToken(req.get('Authorization'));
-
-  if (id == fromUser) {
-    //var student = readDocument('students', id);
-
-    //for (var i = 0, cart=[]; i < student.cart.length; i++) {
-    //cart.push(readDocument('courses', student.cart[i]));
-    //}
-    //res.send(cart);
-  } else {
-    res.status(400).end();
-  }
-});
-
-// GET request for student information
-// Mike is doing this
-app.get('/students/:studentid', function(req, res) {
-  var studentId = new ObjectID(req.params.studentid);
-
-  if (studentId == getUserIdFromToken(req.get('Authorization'))) {
-    db.collection('students').findOne({_id : studentId}, function (err, student) {
-      if (err) sendDatabaseError(res, err);
-      else res.send(student);
-    });
-  }
-});
-
-// MADE CHANGES HERE FIX THIS EVENTUALLY
-//GET request for professor Information
-// Nick is doing this
-app.get('/professor/:professorid', function(req, res) {
-  var professorId = new ObjectID(req.params.professorid);
-
-  db.collection('professors').findOne({_id : professorId}, function (err, professor) {
-    if (err) sendDatabaseError(res, err);
-    else res.send(professor);
-  });
-});
-
-/*
-* Get the user ID from a token. Returns -1 (and invalid ID) if it fails
-*/
-function getUserIdFromToken(authorizationLine) {
-  try {
-    // Cut off "Bearer " from the header value.
-    var token = authorizationLine.slice(7);
-    // Convert the base64 string to a UTF-8 string.
-    var regularString = new Buffer(token, 'base64').toString('utf8');
-    // Convert the UTF-8 string into a JavaScript object.
-    var tokenObj = JSON.parse(regularString);
-    var id = tokenObj['id'];
-    // Check that id is a number.
-    if (typeof id === 'string') {
-      return id;
+    if (id == fromUser) {
+      db.collection('students').findOne({_id : id}, function (err, student) {
+        if (err) {
+          sendDatabaseError(res, err);
+        } else {
+          var courses = [];
+          // A cursor is analogous to a pointer from C/C++. You need to iterate over the cursor object
+          // which is a lot like a LinkedList from Java.
+          var cursor = db.collection('courses').find({_id: {$in : student.enrolledCourses}});
+          cursor.forEach( function(doc) {                 // From the Node.js MongoDB driver API
+            courses.push(doc);                            // forEach takes 2 functions as parameters
+          }, function () {                                // First function is applied every iteration
+            res.send(courses);                            // Second function is applied at the end
+          });
+        }
+      });
     } else {
-      // Not a number. Return -1, an invalid ID.
+      res.status(400).end();
+    }
+  });
+
+  // GET request for student shopping cart
+  app.get('/students/:studentid/cart', function(req, res) {
+    var id = req.params.studentid;
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+
+    if (id == fromUser) {
+      //var student = readDocument('students', id);
+
+      //for (var i = 0, cart=[]; i < student.cart.length; i++) {
+        //cart.push(readDocument('courses', student.cart[i]));
+      //}
+      //res.send(cart);
+    } else {
+      res.status(400).end();
+    }
+  });
+
+  // GET request for student information
+  app.get('/students/:studentid', function(req, res) {
+    var id = new ObjectID(req.params.studentid);
+    var fromUser = getUserIdFromToken(req.get('Authorization'));
+
+    if (id == fromUser) {
+      db.collection('students').findOne({_id : id}, function (err, student) {
+        if (err) {
+          sendDatabaseError(res, err);
+        }
+        else {
+          res.send(student);
+        }
+      });
+    } else {
+      res.status(400).end();
+    }
+  });
+
+  // MADE CHANGES HERE FIX THIS EVENTUALLY
+  //GET request for professor Information
+  app.get('/professor/:professorid', function(req, res) {
+    var id = req.params.professorid;
+    //var professor = readDocument('professor', id);
+    //res.send(professor);
+  });
+
+  /*
+  * Get the user ID from a token. Returns -1 (and invalid ID) if it fails
+  */
+  function getUserIdFromToken(authorizationLine) {
+    try {
+      // Cut off "Bearer " from the header value.
+      var token = authorizationLine.slice(7);
+      // Convert the base64 string to a UTF-8 string.
+      var regularString = new Buffer(token, 'base64').toString('utf8');
+      // Convert the UTF-8 string into a JavaScript object.
+      var tokenObj = JSON.parse(regularString);
+      var id = tokenObj['id'];
+      // Check that id is a number.
+      if (typeof id === 'string') {
+        return id;
+      } else {
+        // Not a number. Return -1, an invalid ID.
+        return -1;
+      }
+    } catch (e) {
+      // Return an invalid ID.
       return -1;
     }
-  } catch (e) {
-    // Return an invalid ID.
-    return -1;
   }
-}
 
-/**
-* Translate JSON Schema Validation failures into error 400s.
-* THIS MUST ALWAYS COME BEFORE app.listen() AND AFTER OUR ROUTES!
-*/
-app.use(function(err, req, res, next) {
-  if (err.name === 'JsonSchemaValidation') {
-    // Set a bad request http response status
-    res.status(400).end();
-  } else {
-    // It's some other sort of error; pass it to next error middleware handler
-    next(err);
-  }
-});
+  /**
+  * Translate JSON Schema Validation failures into error 400s.
+  * THIS MUST ALWAYS COME BEFORE app.listen() AND AFTER OUR ROUTES!
+  */
+  app.use(function(err, req, res, next) {
+    if (err.name === 'JsonSchemaValidation') {
+      // Set a bad request http response status
+      res.status(400).end();
+    } else {
+      // It's some other sort of error; pass it to next error middleware handler
+      next(err);
+    }
+  });
 
-app.listen(3000, function() {
-  console.log('InSpire server listening on port 3000!');
-});
+  app.listen(3000, function() {
+    console.log('InSpire server listening on port 3000!');
+  });
 });
 /******************************************* Nothing goes below here **************************************************/
