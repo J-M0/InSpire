c5a727d7d11c6b1bfeb97249a6663d41ced7d466
Michael Gallant <mgallant@umass.edu>
2016-04-21 13:40:01 -0400

    changed reset database button text

--- client/app/resetdb.js
+++ client/app/resetdb.js
@@ -14,7 +14,7 @@ import React from 'react';
            document.location.reload(false);
          });
          xhr.send();
-     }} style={{color: 'red'}}>Reset Mock DB</a></div>
+     }} style={{color: 'red'}}>Reset DB</a></div>
      );
    }
  }
